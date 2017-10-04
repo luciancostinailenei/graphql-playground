f6e5e2692534ad236df206b4445594095f2050de
Lucian Costin Ailenei <l.ailenei@levi9.com>
2017-10-04 16:34:23 +0300

    Created new CreateLink component;
    Created new CreateLinkMutation;
    Mapped CreateLink component with graphql CreateLinkMutation so that user is able to submit the addition of a new link

--- vegetables-react-relay/src/components/App/App.js
+++ vegetables-react-relay/src/components/App/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
-import LinkListPage         from '../LinkListPage/LinkListPage'
+//import LinkListPage       from '../LinkListPage/LinkListPage'
+import CreateLink           from '../CreateLink/CreateLink'  
+
 
 class App extends Component {
 
   render() {
     return (
-      <LinkListPage />
+      //<LinkListPage />
+      <CreateLink />
     )
   }
   
