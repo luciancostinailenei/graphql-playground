4273bee6e94aa1b1ed15fd4f085cb12962463d63
Lucian Costin Ailenei <l.ailenei@levi9.com>
2017-10-19 11:40:52 +0300

    Updated project.graphcool schema;
    Added new CreateUserMutation;
    Added new SigninUserMutation;
    Updated graphql relay compiled generated files;
    Updated Login Component _confirm function in order to use previous specified mutations for user authentication

--- vegetables-react-relay/src/components/Login/Login.js
+++ vegetables-react-relay/src/components/Login/Login.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component }          from 'react'
+
 import { GC_USER_ID, GC_AUTH_TOKEN } from '../../constants'
+
+import SigninUserMutation            from '../../mutations/SigninUserMutation'
+import CreateUserMutation            from '../../mutations/CreateUserMutation'
 
 class Login extends Component { 
 
@@ -62,8 +66,22 @@ class Login extends Component {
         )
     }
 
-    _confirm = async () => {
-        // ... tbi    
+    _confirm = () => {
+           const { name, email, password } = this.state
+
+           if (this.state.login) {
+               SigninUserMutation(email, password, (id, token) => {
+                   this._saveUserData(id, token)
+                   
+                   this.props.history.push('/')
+               })
+           } else {
+               CreateUserMutation(name, email, password, (id, token) => {
+                   this._saveUserData(id, token)
+
+                   this.props.history.push('/')
+               })
+           }
     }
 
     _saveUserData = (id, token) => {
---
+++ vegetables-react-relay/src/mutations/CreateUserMutation.js
@@ -0,0 +1,61 @@
+import {
+    CommitMutation,
+    graphql
+}                   from 'react-relay'
+
+import environment  from '../Environment'
+
+const mutation = graphql`
+    mutation CreateUserMutation($createUserInput: SignupUserInput!, $signinUserInput: SigninUserInput!) {
+        createUser(input: $createUserInput) {
+            user {
+                id
+            }
+        }
+
+        signinUser(input: $signinUserInput) {
+            token
+            user {
+                id
+            }
+        }
+    }
+`
+
+export default (name, email, password, callback) => {
+    const variables = {
+        createUserInput: {
+            name,
+            authProvider: {
+                email: {
+                    email,
+                    password
+                }
+            },
+            clientMutationId: ''
+        },
+
+        signinUserInput: {
+            email: {
+                email,
+                password
+            },
+            clientMutationId: ''
+        }
+    }
+
+    CommitMutation(
+        environment,
+        {
+            mutation,
+            variables,
+            onCompleted: (response) => {
+                const id = response.createUser.user.id
+                const token = response.signinUser.token
+
+                callback(id, token)
+            },
+            onError: err => console.error(err)
+        }
+    )
+}
